69f70709fa12ac4e4dd042782cfa25d2f9fe9ce3
bhardwajj03 <adi.bhardwaj09@gmail.com>
2024-12-26 16:16:52 +0530

    so much errors

--- app/api/auth/streams/route.ts
+++ app/api/auth/streams/route.ts
@@ -64,3 +64,17 @@ export async function POST(req: NextRequest) {
     );
   }
 }
+
+
+export async function GET(req:NextRequest){
+  const creatorId=req.nextUrl.searchParams.get("creatorId");
+  const streams= await prismaClient.activeStreams.findMany({
+    where:{
+      userId:creatorId ?? ""
+    }
+  })
+
+  return NextResponse.json({
+    streams
+  })
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@
 
 import Image from "next/image";
 import Appbar from "./components/Appbar";
+  
+
+
 
 export default function Home() {
   return (
