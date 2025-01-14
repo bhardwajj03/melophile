2ba5bbdf9f2b8f995a961931e403cb0c4dfda13e
bhardwajj03 <adi.bhardwaj09@gmail.com>
2025-01-14 13:12:08 +0530

    appbar

--- app/components/Appbar.tsx
+++ app/components/Appbar.tsx
@@ -3,27 +3,21 @@
 import { Music } from "lucide-react";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-
+import { Button } from "@/components/ui/button"
 
 export default function Appbar(){
 
     const session=useSession();
-    return <div>
-        <Link className="flex items-center justify-center" href="#">
+    return( 
+    <div >
+      <Link className="flex items-center justify-center p-4" href="/dashboard">
           <Music className="h-6 w-6 mr-2" />
-          <span className="font-bold">FanTune</span>
-        </Link>
-        <nav className="ml-auto flex gap-4 sm:gap-6">
-            <Link className="text-sm font-medium hover:underline underline-offset-4" href="#features">
-              Features
-            </Link>
-            <Link className="text-sm font-medium hover:underline underline-offset-4" href="#how-it-works">
-              How It Works
-            </Link>
-            {session.data?.user && <button className="m-2 p-2 bg-blue-400"  onClick={()=>signOut()}>LogOut</button> }  
-            {!session.data?.user && <button className="m-2 p-2 bg-blue-400"  onClick={()=>signIn()}>SignIn</button> }
-
-        </nav>
-
+          <span className="font-bold">Melophile</span>
+      </Link>
+       <div className="absolute top-4 right-4">
+            {session.data?.user && <Button className="m-2 p-2 bg-blue-500"  onClick={()=>signOut()}>LogOut</Button> }  
+            {!session.data?.user && <Button className="m-2 p-2 bg-blue-500"  onClick={()=>signIn()}>SignIn</Button> }
+       </div>
     </div>
+    )
 }
